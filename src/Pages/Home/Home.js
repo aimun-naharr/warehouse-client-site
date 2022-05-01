e749695384b347b04e0dc41d1551f5703a53d22c
aimunhidden485 <aimunnaharr@gmail.com>
2022-05-01 17:08:34 +0600

    header added

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FirstSection from './First section/FirstSection';
+
+const Home = () => {
+    return (
+        <div>
+            <FirstSection></FirstSection>
+        </div>
+    );
+};
+
+export default Home;
